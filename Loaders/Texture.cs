be6f70b51bc127c45a9bbc89c6990dab292c5c33
asiftastos <asiftastos@gmail.com>
2021-02-10 11:21:45 +0200

    Switched to ImageSharp lib.

--- Loaders/Texture.cs
+++ Loaders/Texture.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using OpenTK.Graphics.OpenGL4;
-using System.Drawing;
-using System.Drawing.Imaging;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
 
 namespace HabiCS.Loaders
 {
@@ -19,13 +21,10 @@ namespace HabiCS.Loaders
 
         public Texture(string filename)
         {
-            Bitmap image = new Bitmap(filename);
+            Image<Rgba32> image = Image.Load<Rgba32>(filename);
 
             width = image.Width;
             height = image.Height;
-
-            BitmapData data = image.LockBits(new Rectangle(0, 0, image.Width, image.Height),
-                ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
 
             ID = GL.GenTexture();
             GL.BindTexture(TextureTarget.Texture2D, ID);
@@ -36,10 +35,21 @@ namespace HabiCS.Loaders
             };
             GL.TexParameterI(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, ref texparam[0]);
             GL.TexParameterI(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, ref texparam[1]);
-            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgb, image.Width, image.Height, 0, 
-                OpenTK.Graphics.OpenGL4.PixelFormat.Rgb, PixelType.UnsignedByte, data.Scan0);
 
-            image.UnlockBits(data);
+            var pixels = new List<byte>(4 * image.Width * image.Height);
+            for (int y = 0; y < image.Height; y++) {
+	            var row = image.GetPixelRowSpan(y);
+
+	            for (int x = 0; x < image.Width; x++) {
+		            pixels.Add(row[x].R);
+		            pixels.Add(row[x].G);
+		            pixels.Add(row[x].B);
+		            pixels.Add(row[x].A);
+	            }
+            }
+
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, image.Width, image.Height, 0, 
+                OpenTK.Graphics.OpenGL4.PixelFormat.Rgba, PixelType.UnsignedByte, pixels.ToArray());
 
             image.Dispose();
         }
