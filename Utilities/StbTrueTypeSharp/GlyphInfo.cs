7390b89584c77b2eb9653c96004091dacd4ce003
asiftastos <asiftastos@gmail.com>
2021-02-14 06:34:03 +0200

    Added SafeStbTrueTypeSharp port.

---
+++ Utilities/StbTrueTypeSharp/GlyphInfo.cs
@@ -0,0 +1,13 @@
+namespace HabiCS.Utilities.StbImageSharp
+{
+    public struct GlyphInfo
+    {
+        public int X;
+        public int Y;
+        public int Width;
+        public int Height;
+		public int XOffset;
+        public int YOffset;
+		public int XAdvance;
+    }
+}
