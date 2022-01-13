6416a183d6cc68bf315068dd7124526b77e7b8d9
asiftastos <asiftastos@gmail.com>
2022-01-13 11:36:39 +0200

    Added new demo for lighting

---
+++ Lighting/Block.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenTK.Graphics.OpenGL4;
+using LGL.Gfx;
+using OpenTK.Mathematics;
+
+namespace Lighting
+{
+    public class Block : IDisposable
+    {
+        private VertexArrayObject _vao;
+        private VertexBuffer _vbo;
+        private VertexBuffer _normalsVbo;
+        private VertexBuffer _ebo;
+        private int elementCount;
+        
+        public Color4 Color { get; set; }
+
+        public Block()
+        {
+            _vao = new VertexArrayObject();
+            _vbo = new VertexBuffer(BufferTarget.ArrayBuffer);
+            _normalsVbo = new VertexBuffer(BufferTarget.ArrayBuffer);
+            _ebo = new VertexBuffer(BufferTarget.ElementArrayBuffer);
+        }
+
+        public void Init()
+        {
+            float[] vertices =
+            {
+                0.0f, 0.0f, 1.0f,
+                1.0f, 0.0f, 1.0f,
+                1.0f, 1.0f, 1.0f,
+                0.0f, 1.0f, 1.0f,
+
+                1.0f, 0.0f, 1.0f,
+                1.0f, 0.0f, 0.0f,
+                1.0f, 1.0f, 0.0f,
+                1.0f, 1.0f, 1.0f,
+                
+                1.0f, 0.0f, 0.0f,
+                0.0f, 0.0f, 0.0f,
+                0.0f, 1.0f, 0.0f,
+                1.0f, 1.0f, 0.0f,
+                
+                0.0f, 0.0f, 0.0f,
+                0.0f, 0.0f, 1.0f,
+                0.0f, 1.0f, 1.0f,
+                0.0f, 1.0f, 0.0f,
+                
+                0.0f, 1.0f, 0.0f,
+                0.0f, 1.0f, 1.0f,
+                1.0f, 1.0f, 1.0f,
+                1.0f, 1.0f, 0.0f,
+                
+                0.0f, 0.0f, 0.0f,
+                1.0f, 0.0f, 0.0f,
+                1.0f, 0.0f, 1.0f,
+                0.0f, 0.0f, 1.0f
+            };
+
+            float[] normals =
+            {
+                0.0f, 0.0f, 1.0f,
+                0.0f, 0.0f, 1.0f,
+                0.0f, 0.0f, 1.0f,
+                0.0f, 0.0f, 1.0f,
+
+                1.0f, 0.0f, 0.0f,
+                1.0f, 0.0f, 0.0f,
+                1.0f, 0.0f, 0.0f,
+                1.0f, 0.0f, 0.0f,
+
+                0.0f, 0.0f, -1.0f,
+                0.0f, 0.0f, -1.0f,
+                0.0f, 0.0f, -1.0f,
+                0.0f, 0.0f, -1.0f,
+
+                -1.0f, 0.0f, 0.0f,
+                -1.0f, 0.0f, 0.0f,
+                -1.0f, 0.0f, 0.0f,
+                -1.0f, 0.0f, 0.0f,
+
+                0.0f, 1.0f, 0.0f,
+                0.0f, 1.0f, 0.0f,
+                0.0f, 1.0f, 0.0f,
+                0.0f, 1.0f, 0.0f,
+
+                0.0f, -1.0f, 0.0f,
+                0.0f, -1.0f, 0.0f,
+                0.0f, -1.0f, 0.0f,
+                0.0f, -1.0f, 0.0f
+            };
+
+            uint[] indices =
+            {
+                0, 1, 2, 0, 2, 3,
+                4, 5, 6, 4, 6, 7,
+                8, 9, 10, 8, 10, 11,
+                12, 13, 14, 12, 14, 15,
+                16, 17, 18, 16, 18, 19,
+                20, 21, 22, 20, 22, 23
+            };
+
+            elementCount = indices.Length;
+
+            _vao.Set();
+            _vbo.Set();
+            _vbo.Data<float>(BufferUsageHint.StaticDraw, vertices, sizeof(float) * 3);
+            _vao.Attributes(new VertexAttribute[] {
+                new VertexAttribute(0, 3, sizeof(float) * 3, 0)
+            }, VertexAttribPointerType.Float);
+            _normalsVbo.Set();
+            _normalsVbo.Data<float>(BufferUsageHint.StaticDraw, normals, sizeof(float) * 3);
+            _vao.Attributes(new VertexAttribute[] {
+                new VertexAttribute(1, 3, sizeof(float) * 3, 0)
+            }, VertexAttribPointerType.Float);
+
+            _ebo.Set();
+            _ebo.Data<uint>(BufferUsageHint.StaticDraw, indices, sizeof(uint));
+        }
+
+        public void Draw()
+        {
+            _vao.Set();
+            _ebo.Set();
+            GL.DrawElements(BeginMode.Triangles, elementCount, DrawElementsType.UnsignedInt, 0);
+        }
+
+        public void Dispose()
+        {
+            _vao.Dispose();
+            _vbo.Dispose();
+            _normalsVbo.Dispose();
+            _ebo.Dispose();
+        }
+    }
+}
---
+++ Lighting/LightingGame.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using LGL.Loaders;
+using LGL.Utilities;
+
+namespace Lighting
+{
+    public class LightingGame : GameWindow
+    {
+        private Shader _shader;
+        private Matrix4 _proj;
+        private OrbitCamera _camera;
+        private Block _block;
+        private Matrix4 _blockModel;
+        private Color4 _lightColor;
+        private const float _ambientStrength = 0.5f;
+        private Vector3 _lightPosition;
+
+        public LightingGame(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings, nativeWindowSettings)
+        {
+            VSync = VSyncMode.On;
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+
+            GL.Enable(EnableCap.DepthTest);
+
+            _shader = Shader.Load("Lighting", 2, "Assets/Shaders/lighting.vert", "Assets/Shaders/lighting.frag");
+            _shader.Use();
+            _shader.SetupUniforms(new string[] { "viewproj", "model", "objectColor", 
+                "lightColor", "ambientStrenth", "lightPos" });
+
+            _proj = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(45.0f), (float)ClientSize.X / (float)ClientSize.Y, 0.1f, 1000.0f);
+
+            _camera = new OrbitCamera(new Vector3(0.0f, 10.0f, 0.0f), new Vector3(0.5f, 1.0f, 0.5f));
+
+            _block = new Block();
+            _block.Init();
+            _block.Color = Color4.Red;
+
+            _blockModel = Matrix4.CreateScale(2.0f);
+
+            _lightColor = Color4.White;
+
+            _lightPosition = new Vector3(4.0f, 4.0f, 4.0f);
+        }
+
+        protected override void OnUnload()
+        {
+            base.OnUnload();
+
+            _shader.Dispose();
+            _block.Dispose();
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+
+            if (KeyboardState.IsKeyDown(Keys.E))
+            {
+                _camera.Rotate(0.5f * args.Time);
+            }
+            if (KeyboardState.IsKeyDown(Keys.Q))
+            {
+                _camera.Rotate(-0.5f * args.Time);
+            }
+            if (KeyboardState.IsKeyDown(Keys.D))
+            {
+                _camera.Move(0.5f * args.Time, _camera.Right);
+            }
+            if (KeyboardState.IsKeyDown(Keys.A))
+            {
+                _camera.Move(-0.5f * args.Time, _camera.Right);
+            }
+            if (KeyboardState.IsKeyDown(Keys.W))
+            {
+                _camera.Move(-0.5f * args.Time, _camera.Forward);
+            }
+            if (KeyboardState.IsKeyDown(Keys.S))
+            {
+                _camera.Move(0.5f * args.Time, _camera.Forward);
+            }
+
+            _camera.Update(args.Time);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            base.OnRenderFrame(args);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            Matrix4 vp = _camera.View * _proj;
+            _shader.Use();
+            _shader.UploadMatrix("viewproj", ref vp);
+            _shader.UploadMatrix("model", ref _blockModel);
+            _shader.UploadColor("objectColor", _block.Color);
+            _shader.UploadColor("lightColor", _lightColor);
+            _shader.UploadFloat("ambientStrenth", _ambientStrength);
+            _shader.UploadVector3("lightPos", _lightPosition);
+            _block.Draw();
+
+            SwapBuffers();
+        }
+
+        protected override void OnKeyUp(KeyboardKeyEventArgs e)
+        {
+            base.OnKeyUp(e);
+
+            if(e.Key == Keys.Escape)
+                Close();
+        }
+    }
+}
